d191a5253acc2dab328088592a4b2ccf091ee206
JeongHyeon Jo <cisxo@naver.com>
2025-10-19 22:43:14 +0900

    feat(user): user info data API

---
+++ common-service/src/main/java/com/donttouch/common_service/auth/entity/vo/MyInfoResponse.java
@@ -0,0 +1,26 @@
+package com.donttouch.common_service.auth.entity.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyInfoResponse {
+    @Id
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "investment_type")
+    private InvestmentType investmentType;
+
+    @Column(name = "name")
+    private String name;
+
+}

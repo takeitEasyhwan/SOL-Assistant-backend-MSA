d1730023d9ece59f6a259ee3e8ce9f156faf596f
JeongHyeon Jo <cisxo@naver.com>
2025-10-20 11:31:30 +0900

    feat(expert): expert API

--- internal-assistant-service/src/main/java/com/donttouch/internal_assistant_service/domain/expert/controller/GuruController.java
+++ internal-assistant-service/src/main/java/com/donttouch/internal_assistant_service/domain/expert/controller/GuruController.java
@@ -2,17 +2,20 @@ package com.donttouch.internal_assistant_service.domain.expert.controller;
 
 
 import com.donttouch.common_service.auth.entity.InvestmentType;
+import com.donttouch.common_service.global.aop.AssignCurrentMemberId;
 import com.donttouch.internal_assistant_service.domain.expert.entity.vo.GuruTradeResponse;
 import com.donttouch.internal_assistant_service.domain.expert.entity.vo.GuruVolumeResponse;
+import com.donttouch.internal_assistant_service.domain.expert.entity.vo.UserTrackingRequest;
+import com.donttouch.internal_assistant_service.domain.expert.entity.vo.UserTrackingResponse;
 import com.donttouch.internal_assistant_service.domain.expert.service.GuruService;
 import com.donttouch.internal_assistant_service.domain.member.entity.Side;
 import com.donttouch.internal_assistant_service.domain.member.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -39,5 +42,12 @@ public class GuruController {
         InvestmentType investmentType = InvestmentType.valueOf(type.toUpperCase());
         GuruTradeResponse response = guruService.getGuruTrade(symbol, investmentType);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/tracking")
+    @AssignCurrentMemberId
+    public ResponseEntity<UserTrackingResponse> tracking(@RequestBody UserTrackingRequest userTrackingRequest) {
+        UserTrackingResponse userTrackingBatchResponse = guruService.collectBatch(userTrackingRequest.getEvents());
+        return new ResponseEntity<>(userTrackingBatchResponse, HttpStatus.CREATED);
     }
 }
